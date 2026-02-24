5a07049a1184d08ea420d232fc0c10c041da4df0
luis-jmtz <luis.jmtz2002@gmail.com>
2026-02-24 14:22:06 -0600

    types table intialized. need to programatically add each type using api

---
+++ SQL/Types_Table.sql
@@ -0,0 +1,4 @@
+Create Table Types(
+id Integer Primary Key Not Null,
+name Text Not Null
+)
--- database_builder.py
+++ database_builder.py
@@ -5,3 +5,17 @@ connection = sqlite3.connect("PokeBase.db")
 
 # initialize cursor to interact with database
 cursor = connection.cursor()
+
+
+
+
+
+"""
+# --------- Intialize Type Table -------- #
+"""
+with open("SQL\Types_Table.sql", 'r') as file:
+    init_types_text = file.read()
+
+cursor.execute(init_types_text)
+
+cursor.close()
